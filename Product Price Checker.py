ca8d7f76cb4be27ced40d1c93ddf55f69f825880
Serkings <serkings.sk@googlemail.com>
2024-11-08 20:49:20 +0000

    Add files via upload

---
+++ Product Price Checker.py
@@ -0,0 +1,11 @@
+#Storing the Price of a ferrari and toyota cars.
+ferrari=420000
+toyota= 21000
+
+#Conditional statement to check which car is more expensive
+if ferrari>toyota:
+    print("Toyota is less expensive.")
+elif ferrari<toyota:
+    print("Ferrari is less expensive.")
+else:
+    print("Both cars are equal in price.")
